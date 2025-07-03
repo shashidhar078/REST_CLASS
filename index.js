80b976fff8f1f18e573ca7bbc9dfd3566027eec0
shashidhar078 <nagunurishashidhar@gmail.com>
2025-07-03 19:10:15 +0530

    ejs is set

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express=require("express");
 const app=express();
+const path=require("path")
 
 const port=8080;
 
 app.listen(port,()=>{
     console.log(`app listens on : ${port}`)
+})
+
+app.set("view engine","ejs")
+app.set(path.join(__dirname,"views"));
+
+app.get("/",(req,res)=>{
+    res.send("You are on main page")
 })
