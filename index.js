75580b96188e7fece6c68059733795a733ed6f7a
shashidhar078 <nagunurishashidhar@gmail.com>
2025-07-05 17:02:02 +0530

    done

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname,"public")))
 
 app.use(express.urlencoded({extended:true}))
 
+//arr is used to replicate it as database
 let posts=[
     {
         username:"apnacollege",
