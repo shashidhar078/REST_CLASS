deb89268652d806b093f57a7189f6fc3e9e3f024
shashidhar078 <nagunurishashidhar@gmail.com>
2025-07-05 12:12:20 +0530

    resolved set and use

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.listen(port,()=>{
 
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"));
-app.use(express.static("public"));
-app.use(express.static(path.join(__dirname,"public")))
+app.set(express.static("public"));
+app.set(express.static(path.join(__dirname,"public")))
 
 app.use(express.urlencoded({extended:true}))
 
